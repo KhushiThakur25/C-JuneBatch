3a48e69b39a89e73590b4033bb241e07ba43ace6
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-06-24 16:34:24 +0530

    Add files via upload

---
+++ Class_1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int n = 65;
+	int a= 23;
+	int b = 25;
+	printf("Sum of a and b is :%d\n",(a+b));
+	printf("Hello world of cpp:%d\n",n);
+	printf("This is 2nd day of our class\n");
+	printf("I'm learning c language for basic implementation..\n");
+	
+	getch();
+}
---
+++ First_program.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	printf("Hello world ..\n");
+	printf("There is c training\t is going on..");
+	getch();
+}
+
